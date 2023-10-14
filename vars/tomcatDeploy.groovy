22d18bd1c80845ea44e94219e0fbfd999a3e9540
suresh997 <44109753+suresh997@users.noreply.github.com>
2023-10-14 12:58:28 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -1,5 +1,5 @@
 def call(ip,cred,warfile){
-sshagent([creds]) {
+sshagent([cred]) {
               // copy war file deploy tomcat server
               sh "scp -o StrictHostKeyChecking=no target/${warfile} ec2-user@${ip}:/opt/tomcat-9/webapps"
               // restart tomcat server
